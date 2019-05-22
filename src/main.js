a820f11089904519a614e41d1d64855eb9f5f184
Erick Celio <erickcelio@hotmail.com>
2019-05-22 18:58:08 -0300

    Implementando login com email e senha

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import firebase from 'firebase'
 import { firebaseConfig } from './../credentials'
 
 import TemplateComponent from './components/Template'
+import Loader from 'vue-spinner/src/RingLoader'
+import Notifications from 'vue-notification'
 
 firebase.initializeApp(firebaseConfig)
 
 Vue.component('TemplateComponent', TemplateComponent)
+Vue.component('Loader', Loader)
+Vue.use(Notifications)
 
 Vue.config.productionTip = false
 
