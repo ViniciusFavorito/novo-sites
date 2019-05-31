e8704ddc1ce2a46b8a202b5aa568ed0e4198723f
ViniciusFavorito7 <tchucubira@gmail.com>
2019-05-31 01:05:49 -0300

    add datepicker

--- src/views/DashBoardEvents.vue
+++ src/views/DashBoardEvents.vue
@@ -8,6 +8,10 @@
       </div>
       <form id='evento' name='evento'>
       <div id="input-container" class="flex-column">
+      <label> Selecione a data do Evento</label>
+
+        <date-picker class='' v-model="time2" type="datetime" :time-picker-options="timePickerOptions"></date-picker>
+
         <md-field>
           <label>Título do Evento</label>
           <md-input v-model="type"></md-input>
@@ -28,33 +32,41 @@
 <script>
 
 import { mapActions } from 'vuex'
+import DatePicker from 'vue2-datepicker'
 
 export default {
-    data () {
+
+    components: { DatePicker },
+  data() {
     return {
+      time1: '',
+      time2: '',
+      time3: '',
       loading: false,
-      title: '',
-      event_content: ''
-    }
-  },  methods: {
-    ...mapActions('CreateEvent', ['CreateEvent']),
-    async defaultLogin () {
-      this.loading = true
-      let type = ''
-      let text = ''
-      const { error, success } = await this.CreateEvent({
-        title: this.title,
-        event_content: this.event_content
-      })
-      if (error) {
-        type = 'error'
-        text = error.message
-      } else {
-        type = 'success'
-        text = success.message
+      // custom lang
+      lang: {
+        days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        pickers: ['next 7 days', 'next 30 days', 'previous 7 days', 'previous 30 days'],
+        placeholder: {
+          date: 'Select Date',
+          dateRange: 'Select Date Range'
+        }
+      },
+      // custom range shortcuts
+      shortcuts: [
+        {
+          text: 'Today',
+          onClick: () => {
+            this.time3 = [ new Date(), new Date() ]
+          }
+        }
+      ],
+      timePickerOptions:{
+        start: '00:00',
+        step: '00:30',
+        end: '23:30'
       }
-      this.$notify({ group: 'app', type, text })
-      this.loading = false
     }
   }
   }
